472ccc95878fa59014887b620daa092b3aa17eec
Megan Yong Rui En <s226280165@deakin.edu.au>
2026-05-19 21:35:36 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-/**
- * ThingSpeak defaults for the web dashboard (matches wifi_config.h).
- * Do not commit to public repos.
- */
-window.THINGSPEAK_DEFAULTS = {
-  channelId: 3384179,
-  readApiKey: "SW96ULW04V7B8TBU",
-  refreshIntervalSec: 5,
-};
